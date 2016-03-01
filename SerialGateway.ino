ea726b5d7b23a38dadc6a5a42aa9371a521998bb
Peter Dey <github@realmtech.net>
2016-03-01 17:38:05 +1100

    Handle erroneous incoming data more reliably. Try to restart the bridge if we think Linino has rebooted.

--- SerialGateway.ino
+++ SerialGateway.ino
@@ -36,6 +36,8 @@ char inputString[MAX_RECEIVE_LENGTH] = "";    // A string to hold incoming comma
 int inputPos = 0;
 boolean commandComplete = false;  // whether the string is complete
 
+unsigned long lastInvalidMessage = 0;
+unsigned int invalidMessages = 0;
 boolean consoleStatus = 0;
 static const char CTRL_C = 3;
 
@@ -45,19 +47,63 @@ void setup()
   gw.begin(RF24_PA_LEVEL_GW, RF24_CHANNEL, RF24_DATARATE, messageCallback);
 }
 
-void bridgeBegin() {
-  delay(2500); //The bare minimum needed to be able to reboot both linino and leonardo.
-               // if reboot fails try increasing this number
-               // The more you run on linux the higher this number should be
-  Serial1.begin(250000); // Set the baud.
-  // Wait for U-boot to finish startup.  Consume all bytes until we are done.
+void purgeSerialInput() {
   do {
     while (Serial1.available() > 0) {
       Serial1.read();
     }
     delay(1000);
   } while (Serial1.available()>0);
+}
 
+int strcmp2(char *s, char *t) {
+  int i, j;
+  	
+  for (i=0, j=0; s[i] || t[i]; s[i]==t[i] ? i++ : *s++)
+    j++;
+  		
+  return (i > 0) && (i == j);
+}
+
+int waitForShell() {
+  char input[64] = "";
+  int i = 0;
+  int rounds = 0;
+  
+  while ( 1 ) {
+    rounds++;
+    Serial1.print(F("\n"));
+    delay(250);
+    while (Serial1.available() > 0) {
+      input[i] = Serial1.read();
+      
+      if (input[i] == '\n') {
+        input[i] = 0;
+        inputPos = 0;
+      }
+      
+      input[i+1] = 0;
+      if (strcmp("root@Micromark:~# ", input) || strcmp("root@Micromark:/# ", input)) {
+          return rounds;
+      }
+      
+      i++;
+      if (i == 63)
+        i=0;
+    }
+    delay(500);
+  }
+}
+
+void bridgeBegin() {
+  int rounds = 0;
+  delay(2500); //The bare minimum needed to be able to reboot both linino and leonardo.
+               // if reboot fails try increasing this number
+               // The more you run on linux the higher this number should be
+  Serial1.begin(250000); // Set the baud.
+  // Wait for U-boot to finish startup.  Consume all bytes until we are done.
+  purgeSerialInput();
+  
   // Kill the Arduino bridge in case it's running
   Serial1.write((uint8_t *)"\xff\0\0\x05XXXXX\x0d\xaf", 11);
   Serial1.print("\n");
@@ -68,15 +114,16 @@ void bridgeBegin() {
   delay(250);
   Serial1.print(F("\n"));
   delay(250);
-  while (Serial1.available() > 0) {
-    Serial1.read();
-  }
+  
+  purgeSerialInput();
+  rounds += waitForShell();
   
   // Start up our own bridge
   Serial1.print("/mnt/sda1/newbridge/newbridge.py -q -l /mnt/sda1/newbridge/log.log\n");
-  while (Serial1.available() > 0) {
-    Serial1.read();
-  }
+  delay(1000);
+  purgeSerialInput();
+  Serial1.print("0;255;3;0;9;Shell rounds: ");
+  Serial1.println(rounds);
 }
 
 void loop()  
@@ -94,8 +141,38 @@ void loop()
   gw.processRadioMessage();
   if (commandComplete) {
     // A command wass issued from serial interface
-    // We will now try to send it to the actuator
-    gw.parseAndSend(inputString);
+    
+    // Send if it's a valid message; assume the Bridge has failed.  Restart the bridge.
+    if (gw.isValidMessage(inputString))
+      gw.parseAndSend(inputString);
+    else {
+      if ((lastInvalidMessage+1000) < millis()) {
+        invalidMessages = 0;
+      }
+      
+      Serial1.print("0;255;3;0;9;");
+      Serial1.print(float(millis())/1000.0);
+      Serial1.print("s Invalid message [");
+      Serial1.print(invalidMessages);
+      Serial1.print("/-");
+      Serial1.print(float(millis()-lastInvalidMessage)/1000.0);
+      Serial1.print("s]: ");
+      Serial1.print(inputString);
+      Serial1.print(F("\n"));
+      
+      invalidMessages++;
+      if ((lastInvalidMessage+1000) > millis()) {
+        if (invalidMessages > 2) {
+          Serial1.flush();
+          Serial1.end();
+          delay(10000);
+          bridgeBegin();
+        }
+      } else {
+        lastInvalidMessage = millis();
+      }
+    }
+    
     commandComplete = false;  
     inputPos = 0;
   }
